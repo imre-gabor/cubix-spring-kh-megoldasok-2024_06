be7f96e29069746da2e8eab1d0c96f113023dbe9
imre-gabor <gabor@aut.bme.hu>
2024-07-25 16:52:43 +0200

    prepare for live 4

--- university-fe/src/app/course-chat/course-chat.component.ts
+++ university-fe/src/app/course-chat/course-chat.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import * as SockJS from 'sockjs-client';
-import { Stomp } from '@stomp/stompjs';
+import { Client, StompHeaders } from '@stomp/stompjs';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-course-chat',
@@ -11,13 +11,15 @@ import { Stomp } from '@stomp/stompjs';
 export class CourseChatComponent implements OnInit {
 
   courseId: Number;
-  stompClient;
+  stompClient: Client;
   sender: string;
   text: string;
   chatMessages: string[] = [];
 
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
     this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
@@ -25,21 +27,32 @@ export class CourseChatComponent implements OnInit {
   }
 
   connect() {
-    const ws = new SockJS('/api/stomp');
-    this.stompClient = Stomp.over(ws);    
-    this.stompClient.connect({}, frame => {   
-      console.log('Connected: ' + frame);
-      this.subscribeToCourseChat();
+    //const ws = new SockJS('/api/stomp');
+    //this.stompClient = Stomp.over(ws);
+    // this.stompClient.connect({'X-Authorization' : 'Bearer ' + this.authService.getToken()}, frame => {   
+    //   console.log('Connected: ' + frame);
+    //   this.subscribeToCourseChat();
+    // });
+
+    this.stompClient = new Client({
+      brokerURL: '/api/stomp',
+      connectHeaders: {'X-Authorization' : 'Bearer ' + this.authService.getToken()},
+      onConnect: () => {
+        this.subscribeToCourseChat();
+      }
     });
-    
+    this.stompClient.activate();
   }
 
   sendMessage() {
-    this.stompClient.send('/app/chat', {}, JSON.stringify({
-      "sender": this.sender,
-      "courseId": this.courseId,
-      "text": this.text
-    }));
+    this.stompClient.publish({
+      "destination": '/app/chat',
+      "body": JSON.stringify({
+        "sender": this.sender,
+        "courseId": this.courseId,
+        "text": this.text
+      })
+    });      
   }
 
   subscribeToCourseChat() {
